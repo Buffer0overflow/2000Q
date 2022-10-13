d9e188e9bd331957b42915454336fe1a0d4d0295
Buffer Overflow <project1hacker@gmail.com>
2022-10-14 02:47:57 +0300

    FizzBuss problem solution

---
+++ search Algorithms/CommonProblems/fizzBizz.c
@@ -0,0 +1,17 @@
+/*
+Fizz Buzz Problem
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 1 ; i <= 17 ;i++ )
+    {
+        if (i % 3 == 0 && i % 5 == 0) printf("[%d] : FizzBuzz\n",i);
+        else if ( i % 5 == 0) printf("[%d] : Buzz\n",i);
+        else if (i % 3 == 0) printf("[%d] : Fizz\n",i);
+        else printf("%d\n",i);
+    }
+    return 0;
+}
