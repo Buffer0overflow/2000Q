3ef741850744733ed28dafa71ce8c70992e47fb2
Buffer Overflow <project1hacker@gmail.com>
2022-11-01 18:24:40 +0300

    add #ifdef to make code portable to work on win and POSIX

--- Files/Reverse_file.c
+++ Files/Reverse_file.c
@@ -7,7 +7,7 @@ Q67: Reverse A File on Windows OS.
 
 int main(int argc, char const *argv[])
 {
-    FILE *fp = fopen("file.txt","r");
+    FILE *fp = fopen("G:\\file.txt","r");
     if (NULL == fp)
     {
         puts("Error opening file");
@@ -17,7 +17,9 @@ int main(int argc, char const *argv[])
     fseek(fp,0,SEEK_END); 
     int len = ftell(fp); 
     printf("Length at end is %ld\n",len);
-    fseek(fp,-1,SEEK_CUR);  
+    fseek(fp,-1,SEEK_CUR); 
+
+    #ifdef _WIN32 // to make it portable code , we need to check OS running so we take care of \n when reverse the file
     while ( len > 0)
     {
         char c = fgetc(fp);
@@ -28,6 +30,15 @@ int main(int argc, char const *argv[])
         fseek(fp,-2,SEEK_CUR);
         len--;
     }
+    #else
+	    while ( len > 0)
+    {
+        char c = fgetc(fp);
+        printf("%c",c);
+        fseek(fp,-2,SEEK_CUR);
+        len--;
+    }
+	#endif
     
     fclose(fp);
     return 0;
